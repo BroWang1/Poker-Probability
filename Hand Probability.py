654130aa5c9c71b53beaf45700aff16dcba55cfd
BroWang1 <brodeywang2004@gmail.com>
2024-12-18 12:58:11 -0500

    adding Deck.py and Hand Probability.py

--- Hand Probability.py
+++ Hand Probability.py
@@ -1,11 +1,14 @@
 from Deck import deck, card2rank,card2suit          #i wanted to test out how this import works from different .py files
-import random
 import math
+
+community_cards = []
+suit_community_cards = []
+rank_community_cards = []
 
                                                     #to get initial info like players and hand
 num_players = input("How many players are playing? (Please give numerical values)" + "\n")
 otherp_cards = (int(num_players)-1)*2                                # every player has 2 cards
-user_input = input("Enter Hand(EX.QH,5C - Queen of Hearts, 5 of Clubs):" + "\n").upper()
+user_input = input("Enter Hand(EX.6H,5C - Six of Hearts, 5 of Clubs):" + "\n").upper()
 user_hand = user_input.replace(" ", "").split(",")      # cleaning/normalizing the answers
 print(user_hand)
 
@@ -18,10 +21,6 @@ suit_hand = [card2suit(card) for card in user_hand]
 print(suit_hand)
 rank_hand = [card2rank(card) for card in user_hand]
 print(rank_hand)
-
-community_cards = []
-suit_community_cards = []
-rank_community_cards = []
 
 unseen_cards = len(deck) - len(community_cards)
 total_combo = math.comb(unseen_cards, otherp_cards)  # the denominator
@@ -39,28 +38,36 @@ else:
 rank_board = rank_hand + rank_community_cards
 suit_board = suit_hand + suit_community_cards
 
-lowerC_deck = (len(deck) - amt_higher)
-def high_card(rank_hand, community_cards, rank_community_cards):
-    if not community_cards:     #preflop
-        high_card = math.comb((lowerC_deck), otherp_cards) / total_combo
-        return high_card
-    # comb1 = math.comb(lowerC_deck, otherp_cards) # combo number of cards lower to the number of other players cards
-    # comb2 = math.comb(lowerC_deck - otherp_cards, 5 - len(community_cards))
-
-
-    elif community_cards:       #postflop
-        high_rankin_hand = max(rank_hand) # highest card in hand
-        num_higherCC = sum(1 for rank in rank_community_cards if rank > high_rankin_hand)
-        num_lowerCC = len(rank_community_cards) - num_higherCC
-        if any(rank_board.count(card) > 1 for card in rank_board):                    #using a comprehension would make it better
+lowerC_deck = len(deck) - amt_higher
+def high_card(rank_hand, suit_community_cards, rank_community_cards):
+    rank_board = rank_hand + rank_community_cards
+    suit_board = suit_hand + suit_community_cards
+    if any(rank_board.count(rank) >= 2 for rank in set(rank_board)):      #Checking for pairs, trips, quads
+        print('0% - Better Hand Available')
+        return
+    elif any(suit_board.count(card) >= 5 for card in suit_board):      #Checking for flushes
+        print('0% - Better Hand Available')
+        return
+    elif len(rank_board) >= 5:
+        sort = sorted(rank_board)
+        if any(
+                sort[i + 1] == sort[i] + 1 and
+                sort[i + 2] == sort[i] + 2 and
+                sort[i + 3] == sort[i] + 3 and
+                sort[i + 4] == sort[i] + 4
+            for i in range(len(rank_board) - 4)
+            ):
             print('0% - Better Hand Available')
-        else:
-            high_card = math.comb((len(deck) - num_lowerCC - amt_higher + num_higherCC), otherp_cards - num_higherCC) / total_combo
-            return high_card
+            return
 
+    high_rankin_hand = max(rank_hand)  # highest card in hand
+    num_higherCC = sum(1 for rank in rank_community_cards if rank > high_rankin_hand)
+    num_lowerCC = len(rank_community_cards) - num_higherCC
+    possible_lowerC = math.comb((lowerC_deck - num_lowerCC + num_higherCC), otherp_cards)
+    high_card = possible_lowerC / total_combo
+    return high_card
 
-
-print(high_card(rank_hand,community_cards, rank_community_cards))
+print(high_card(rank_hand,suit_community_cards, rank_community_cards))
 
 
 # def evaluate_hand(value_hand, value_community_cards):
@@ -97,14 +104,31 @@ print(high_card(rank_hand,community_cards, rank_community_cards))
     # roy_flush =
     #
     # total_prob = high_card + pair + two_pair + trips + quads + straight + flush + full_house + str_flush + roy_flush
-input_community_cards = input("What are the first 3 community cards?(EX.2H,8C,7D)" + "\n").upper()
-community_cards = input_community_cards.replace(" ", "").split(",")
-for knownC in range(len(community_cards)):                                       # known cards in the community
-     randomly_rem = random.choice(deck)
-     deck.remove(randomly_rem)
-print(len(deck))
 
+def update_community_cards(input_cards, community_cards, deck):
+    new_cards = input_cards.replace(" ", "").split(",")
+    community_cards += new_cards
+    for card in new_cards:
+        if card in deck:
+            deck.remove(card)
+    return community_cards, deck
 
-rank_community_cards = [card2rank(card) for card in community_cards]# cleaning/normalizing the answers
-postflop_high_card = high_card(rank_hand,community_cards, rank_community_cards)
+input_community_cards = input("Enter the first 3 community cards (e.g., 9H, 8C, 7D): " + "\n").upper()
+community_cards, deck = update_community_cards(input_community_cards, community_cards, deck)
+
+suit_community_cards = [card2suit(card) for card in community_cards]
+rank_community_cards = [card2rank(card) for card in community_cards]
+
+postflop_high_card = high_card(rank_hand, suit_community_cards, rank_community_cards)
 print(postflop_high_card)
+
+# Repeat for 4th and 5th cards
+for round_num in range(4, 6):
+    input_card = input(f"Enter the {round_num}th community card: " + "\n").upper()
+    community_cards, deck = update_community_cards(input_card, community_cards, deck)
+
+    suit_community_cards = [card2suit(card) for card in community_cards]
+    rank_community_cards = [card2rank(card) for card in community_cards]
+
+    probability = high_card(rank_hand, suit_community_cards, rank_community_cards)
+    print(probability)
